71ae60538bcc2d0a02e409474e4d57e08253c1d5
gagest <130384485+GageSt@users.noreply.github.com>
2023-12-15 13:48:22 -0700

    Fix base url

--- OfficeDrone/vite.config.ts
+++ OfficeDrone/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/OfficeDrone/',
+  base: 'http://gagest.github.io/OfficeDrone/',
   plugins: [
     vue(),
   ],
