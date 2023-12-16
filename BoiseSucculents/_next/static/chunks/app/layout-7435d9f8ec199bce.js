7eae6b422e1497646348941cb8826dc671437742
gagest <130384485+GageSt@users.noreply.github.com>
2023-12-15 17:17:37 -0700

    Changed BoiseSucculents

---
+++ BoiseSucculents/_next/static/chunks/app/layout-7435d9f8ec199bce.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{67:function(n,e,t){Promise.resolve().then(t.t.bind(t,5005,23)),Promise.resolve().then(t.t.bind(t,2445,23))},2445:function(){},5005:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=67)}),_N_E=n.O()}]);
